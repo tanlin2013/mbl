0889e6f64d1c875aa6537bc8390ba64c66997208
taolin <tanlin2013@gmail.com>
2021-09-07 13:55:46 +0800

    Refactor: add entropy plots & performance improvement

--- scripts/app.py
+++ scripts/app.py
@@ -1,6 +1,9 @@
+import orjson
 import pandas as pd
 import streamlit as st
 import plotly.figure_factory as ff
+import plotly.io as pio
+from itertools import count
 from pathlib import Path
 from mbl.level_statistic import (
     LevelStatistic,
@@ -8,21 +11,42 @@ from mbl.level_statistic import (
 )
 
 
+pio.renderers.default = 'iframe'
+
+
 @st.cache(persist=True)
 def load_data():
-    raw_df = pd.read_csv(f'{Path(__file__).parent}/random_heisenberg_config.csv')
+    raw_df = pd.read_parquet('s3://many-body-localization-random-heisenberg/config/random_heisenberg_config.parquet')
+    # raw_df = pd.read_parquet(f'{Path(__file__).parents[1]}/data/random_heisenberg_config.parquet')
     return LevelStatistic(raw_df)
+
+
+@st.cache(persist=True)
+def density_histogram_2d(df: pd.DataFrame, x: str, y: str, title: str):
+    fig = ff.create_2d_density(
+        df[x], df[y],
+        title=title,
+        point_size=3
+    )
+    fig.update_layout(
+        xaxis_title=x,
+        yaxis_title=y
+    )
+    return fig
 
 
 if __name__ == "__main__":
 
     agent = load_data()
 
-    N = 10
-    h = st.sidebar.radio('Disorder strength h', (1.0, 4.0))
+    N = st.sidebar.radio('System size N', (8, 10))
+    h = st.sidebar.radio('Disorder strength h', (0.5, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 10.0))
     total_sz = st.sidebar.radio("Restrict TotalSz in", (None, 0, 1))
-    n_conf = st.sidebar.radio("Number of disorder trials "
-                              "(can't be too large due to limited memory for making plots)", (2, 4, 6, 8))
+    n_conf = st.sidebar.radio(
+        "Number of disorder trials "
+        "(can't be too large due to limited memory for making plots)",
+        (4, 6, 8, 10)
+    )
     df = agent.extract_gap(N, h, total_sz=total_sz).query(f'TrialID < {n_conf}')
 
     st.markdown('# Level statistics of Heisenberg model with random transversed field')
@@ -36,31 +60,19 @@ if __name__ == "__main__":
     st.markdown('#')
     st.dataframe(df)
 
+    plot_pairs = [
+        ('En', 'LevelID'),
+        ('GapRatio', 'En'),
+        ('En', 'EntanglementEntropy'),
+        ('GapRatio', 'EntanglementEntropy'),
+        ('En', 'EnergyGap'),
+        ('En', 'TotalSz'),
+        ('GapRatio', 'TotalSz')
+    ]
+
     st.markdown('#')
-    fig = ff.create_2d_density(
-        df['En'], df['LevelID'],
-        title='Fig 1. En(x-axis) - Level(y-axis)',
-        point_size=3
-    )
-    st.write(fig)
-
-    fig = ff.create_2d_density(
-        df['GapRatio'], df['En'],
-        title='Fig 2. GapRatio(x-axis) - En(y-axis)',
-        point_size=3
-    )
-    st.write(fig)
-
-    fig = ff.create_2d_density(
-        df['En'], df['TotalSz'],
-        title='Fig 3. En(x-axis) - TotalSz(y-axis)',
-        point_size=3
-    )
-    st.write(fig)
-
-    fig = ff.create_2d_density(
-        df['GapRatio'], df['TotalSz'],
-        title='Fig 4. GapRatio(x-axis) - TotalSz(y-axis)',
-        point_size=3
-    )
-    st.write(fig)
+    for k, v in zip(count(start=1), plot_pairs):
+        if not ('TotalSz' in v and total_sz is not None):
+            st.write(
+                density_histogram_2d(df, v[0], v[1], f'Fig. {k}: {v[0]}(xaxis) - {v[1]}(yaxis)')
+            )
