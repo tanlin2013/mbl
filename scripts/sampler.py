f677bfa4707ac19a00aad284c8e58ba6a3c995e7
taolin <tanlin2013@gmail.com>
2022-01-29 02:06:04 +0800

    Config: update job script

--- scripts/sampler.py
+++ scripts/sampler.py
@@ -1,25 +1,19 @@
-import sys
-import logging
+import ray
 import pandas as pd
+# import awswrangler as wr
 from pathlib import Path
 from dask_jobqueue import SLURMCluster
-try:
-    from mbl.distributed import Distribute
-except ImportError:
-    sys.path.append(str(Path(__file__).parents[1]))
-    from mbl.distributed import Distribute
+from mbl.model import RandomHeisenbergED, RandomHeisenbergTSDRG
+from mbl.distributed import Distribute
 
 
 def main1(kwargs) -> pd.DataFrame:
-    from mbl.model import RandomHeisenbergED
     agent = RandomHeisenbergED(**kwargs)
     return agent.df
 
 
+@ray.remote(memory=10 * 1024 ** 3)
 def main2(kwargs) -> pd.DataFrame:
-    # import awswrangler as wr
-    from mbl.model import RandomHeisenbergTSDRG
-
     agent = RandomHeisenbergTSDRG(**kwargs)
     # agent.save_tree("")
     df = agent.df
@@ -54,7 +48,7 @@ if __name__ == "__main__":
     penalty = 0.0
     s_target = 0
     offset = 0.0
-    n_conf = 10
+    n_conf = 500
 
     params = [
         {
@@ -67,22 +61,22 @@ if __name__ == "__main__":
             's_target': s_target,
             'offset': offset
         }
-        for n in [8, 10]
+        for n in [8, 10, 12, 14, 16]
         for h in [0.5, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 10.0]
         for chi in [2**4, 2**6, 2**8]
         for trial_id, seed in enumerate(range(1900, 1900 + n_conf))
     ]
 
-    cluster = scopion()
-    logging.info(cluster.job_script())
-    cluster.adapt(
-        minimum=4,
-        maximum=48,
-        target_duration="1200",  # measured in CPU time per worker -> 120 seconds at 10 cores / worker
-        wait_count=4  # scale down more gently
-    )
-    results = Distribute.map_on_dask(main2, params, cluster)
+    # cluster = scopion()
+    # print(cluster.job_script())
+    # cluster.adapt(
+    #     minimum=4,
+    #     maximum=48,
+    #     target_duration="1200",  # measured in CPU time per worker -> 120 seconds at 10 cores / worker
+    #     wait_count=4  # scale down more gently
+    # )
+    results = Distribute.map_on_ray(main2, params)
     # wr.catalog.table(database="awswrangler_test", table="noaa")
     merged_df = pd.concat(results)
-    merged_df.to_parquet(f'~/data/random_heisenberg_tsdrg.parquet', index=False)
-    # merged_df.to_parquet(f'{Path(__file__).parents[1]}/data/random_heisenberg_tsdrg.parquet', index=False)
+    # merged_df.to_parquet(f'~/data/random_heisenberg_tsdrg.parquet', index=False)
+    merged_df.to_parquet(f'{Path(__file__).parents[1]}/data/random_heisenberg_tsdrg.parquet', index=False)
