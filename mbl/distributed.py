5bc5444076e36ef32c5d89ca1f00de5691210975
taolin <tanlin2013@gmail.com>
2022-02-13 04:17:20 +0800

    Test: switch back to chunk

--- mbl/distributed.py
+++ mbl/distributed.py
@@ -1,6 +1,6 @@
 import ray
 import psutil
-# import numpy as np
+import numpy as np
 from tqdm import tqdm
 from itertools import islice
 from ray.remote_function import RemoteFunction
@@ -17,14 +17,13 @@ class Distributed:
         return cpu_load_1 / psutil.cpu_count() / (mem.available * 1e-9)
 
     @staticmethod
-    def map_on_ray(func: Callable, params: Sequence, chunk_size: int = 32, max_workload: float = 0.5) -> List:
+    def map_on_ray(func: Callable, params: Sequence, chunk_size: int = 7) -> List:
         """
 
         Args:
             func:
             params:
             chunk_size:
-            max_workload:
 
         Returns:
 
@@ -42,26 +41,10 @@ class Distributed:
             ray.init()
         func = ray.remote(func) if not isinstance(func, RemoteFunction) else func
         results = []
-        # for chunk_params in tqdm(chunk(params), desc='All chunks', total=int(np.ceil(len(params) / chunk_size))):
-        #     jobs = [func.remote(i) for i in chunk_params]
-        #     for done_job in tqdm(watch(jobs), desc='Current chunk', position=1, total=len(jobs)):
-        #         results += [done_job]
-
-        def update(results, pbar):
-            for done_job in watch(jobs):
+        for chunk_params in tqdm(chunk(params), desc='All chunks', total=int(np.ceil(len(params) / chunk_size))):
+            jobs = [func.remote(i) for i in chunk_params]
+            for done_job in tqdm(watch(jobs), desc='Current chunk', position=1, total=len(jobs)):
                 results += [done_job]
-                pbar.update(1)
-
-        inputs = iter(params)
-        jobs = [func.remote(next(inputs)) for _ in range(chunk_size)]
-        with tqdm(desc='Completed jobs', total=len(params)) as pbar:
-            update(results, pbar)
-            while len(params) > len(jobs):
-                if Distributed.get_workload() < max_workload:
-                    jobs += [func.remote(next(inputs))]
-                else:
-                    update(results, pbar)
-            update(results, pbar)
         ray.shutdown()
         return results
 
