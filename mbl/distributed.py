45e6931857e9d4c878c42017bce1c7b24b309ce9
taolin <tanlin2013@gmail.com>
2022-02-13 21:27:43 +0800

    Refactor: rm chunk

--- mbl/distributed.py
+++ mbl/distributed.py
@@ -1,8 +1,5 @@
 import ray
-import psutil
-# import numpy as np
 from tqdm import tqdm
-from itertools import islice
 from ray.remote_function import RemoteFunction
 from dask.distributed import Client, progress
 from typing import Callable, Sequence, List
@@ -11,27 +8,16 @@ from typing import Callable, Sequence, List
 class Distributed:
 
     @staticmethod
-    def get_workload():
-        cpu_load_1, _, _ = psutil.getloadavg()
-        mem = psutil.virtual_memory()
-        return cpu_load_1 / psutil.cpu_count() / (mem.available * 1e-9)
-
-    @staticmethod
-    def map_on_ray(func: Callable, params: Sequence, chunk_size: int = 7) -> List:
+    def map_on_ray(func: Callable, params: Sequence) -> List:
         """
 
         Args:
             func:
             params:
-            chunk_size:
 
         Returns:
 
         """
-        def chunk(inputs):
-            inputs = iter(inputs)
-            return iter(lambda: list(islice(inputs, chunk_size)), [])
-
         def watch(obj_ids):
             while obj_ids:
                 done, obj_ids = ray.wait(obj_ids)
@@ -40,11 +26,10 @@ class Distributed:
         if not ray.is_initialized:
             ray.init()
         func = ray.remote(func) if not isinstance(func, RemoteFunction) else func
-        results = []
-        # for chunk_params in tqdm(chunk(params), desc='All chunks', total=int(np.ceil(len(params) / chunk_size))):
         jobs = [func.remote(i) for i in params]
-        for done_job in tqdm(watch(jobs), total=len(jobs)):
-            results += [done_job]
+        results = []
+        for done_job in tqdm(watch(jobs), desc='Completed jobs', total=len(jobs)):
+            results.append(done_job)
         ray.shutdown()
         return results
 
