aee300fad34c5a0300836164b96968c30cb838ac
taolin <tanlin2013@gmail.com>
2022-04-13 01:12:12 +0800

    Feature: compute gap_ratio in advance to sql

---
+++ scripts/gap_ratio.py
@@ -0,0 +1,69 @@
+import ray
+import pandas as pd
+import awswrangler as wr
+from botocore.exceptions import ClientError
+from time import sleep
+from mbl.name_space import Columns
+from mbl.distributed import Distributed
+from mbl.level_statistic import (
+    LevelStatistic,
+    AverageOrder
+)
+
+
+def retry(func, *args, **kwargs):
+    for attempt in range(3):
+        try:
+            func(*args, **kwargs)
+            break
+        except ClientError:
+            sleep(3)
+
+
+@ray.remote(num_cpus=1)
+def fetch_gap_ratio(n: int, h: float, penalty: float = 0, s_target: int = 0,
+                    offset: float = 0, chi: int = None, total_sz: int = None):
+    df = LevelStatistic().extract_gap(**locals())
+    df = pd.DataFrame(
+        {
+            Columns.system_size: [n],
+            Columns.disorder: [h],
+            Columns.penalty: [penalty],
+            Columns.s_target: [s_target],
+            Columns.truncation_dim: [chi],
+            Columns.total_sz: [total_sz],
+            Columns.offset: [offset],
+            Columns.gap_ratio: [LevelStatistic.averaged_gap_ratio(df)]
+        }
+    )
+    retry(
+        wr.s3.to_parquet,
+        df=df,
+        path="s3://many-body-localization/gap_ratio",
+        index=False,
+        dataset=True,
+        mode="append",
+        database="random_heisenberg",
+        table="gap_ratio"
+    )
+
+
+if __name__ == "__main__":
+
+    params = [
+        {
+            'n': n,
+            'h': h,
+            'chi': chi,
+            'penalty': 0,
+            's_target': 0,
+            'offset': 0
+        }
+        for n in [8, 10, 12, 14, 16, 18, 20]
+        for chi in [2 ** 3, 2 ** 4, 2 ** 5, 2 ** 6]
+        for h in [0.5, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 10.0]
+    ]
+
+    ray.init(num_cpus=16)
+    results = Distributed.map_on_ray(fetch_gap_ratio, params)
+
