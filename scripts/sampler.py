7208c60bf23d646406d0806855d5a2ff6de9f53a
taolin <tanlin2013@gmail.com>
2022-05-14 16:09:13 +0800

    Chore: update script with ray tune

--- scripts/sampler.py
+++ scripts/sampler.py
@@ -1,164 +1,94 @@
+import os
+import click
+
+import yaml
 import ray
-import pandas as pd
-import awswrangler as wr
-from botocore.exceptions import ClientError
-from pathlib import Path
-from time import sleep
-# from dask.distributed import LocalCluster
+from ray import tune
+
 from mbl.name_space import Columns
-from mbl.experiment import RandomHeisenbergED, RandomHeisenbergTSDRG
-from mbl.distributed import Distributed
-from mbl.level_statistic import LevelStatistic
-
-
-def retry(func, *args, **kwargs):
-    for attempt in range(3):
-        try:
-            func(*args, **kwargs)
-            break
-        except ClientError:
-            sleep(3)
-
-
-@ray.remote(num_cpus=1, memory=0.8 * 1024 ** 3)
-def main1(kwargs) -> pd.DataFrame:
-    agent = RandomHeisenbergED(**kwargs)
-    df = agent.df
-    retry(
-        wr.s3.to_parquet,
-        df=df,
-        path="s3://many-body-localization/dataframe/ed",
-        dataset=True,
-        mode="append",
-        partition_cols=[
-            Columns.system_size,
-            Columns.disorder,
-            Columns.penalty,
-            Columns.s_target,
-            Columns.offset
-        ],
-        database="random_heisenberg",
-        table="ed2"
-    )
-    return df
-
-
-@ray.remote(num_cpus=1)
-def main2(kwargs):
-    print(kwargs)
-    agent = RandomHeisenbergTSDRG(**kwargs)
-    df = agent.df
-    retry(
-        wr.s3.to_parquet,
-        df=df,
-        path="s3://many-body-localization/dataframe/tsdrg",
-        index=False,
-        dataset=True,
-        mode="append",
-        partition_cols=[
-            Columns.system_size,
-            Columns.disorder,
-            Columns.truncation_dim,
-            Columns.penalty,
-            Columns.s_target,
-            Columns.offset
-        ],
-        database="random_heisenberg",
-        table="tsdrg2"
-    )
-    path = Path(f"{Path(__file__).parents[1]}/data/tree")
-    path.mkdir(parents=True, exist_ok=True)
-    filename = "-".join([f"{k}_{v}" for k, v in kwargs.items()])
-    agent.save_tree(f"{path}/{filename}")
-    del agent
-    # return df
-
-
-@ray.remote(num_cpus=1)
-def main3(kwargs):
-    df = LevelStatistic().extract_gap(
-        n=kwargs.get('n'), h=kwargs.get('h'), seed=kwargs.get('seed'),
-        chi=kwargs.get('chi'),
-    )
-    if len(df.index) < kwargs.get('chi'):
-        agent = RandomHeisenbergTSDRG(**kwargs)
-        df = agent.df
-        retry(
-            wr.s3.to_parquet,
-            df=df,
-            path="s3://many-body-localization/dataframe/tsdrg",
-            index=False,
-            dataset=True,
-            mode="append",
-            partition_cols=[
-                Columns.system_size,
-                Columns.disorder,
-                Columns.truncation_dim,
-                Columns.penalty,
-                Columns.s_target,
-                Columns.offset
-            ],
-            database="random_heisenberg",
-            table="tsdrg2"
-        )
-        path = Path(f"{Path(__file__).parents[1]}/data/tree")
-        path.mkdir(parents=True, exist_ok=True)
-        filename = "-".join([f"{k}_{v}" for k, v in kwargs.items()])
-        agent.save_tree(f"{path}/{filename}")
+from mbl.pipeline.grid_search import (
+    RandomHeisenbergTSDRGGridSearch,
+    RandomHeisenbergFoldingTSDRGGridSearch,
+)
+from mbl.analysis.energy_bounds import EnergyBounds
 
 
 def scopion():
     from dask_jobqueue import SLURMCluster
+
     return SLURMCluster(
         cores=32,
         memory="10G",
         processes=30,
-        queue='scopion1',
+        queue="scopion1",
         walltime="00:30:00",
         header_skip=["--mem"],
         scheduler_options={"host": "192.168.1.254"},
         # host='192.168.1.254',
         # extra=['--no-dashboard'],
-        env_extra=['module load singularity', ],  # ensure singularity is loaded
+        env_extra=["module load singularity", ],  # ensure singularity is loaded
         python=f"singularity run mbl.sif python",  # use python in container
     )
 
 
+def get_config():
+    with open("run_config.yml", "r") as f:
+        data = yaml.load(f)
+    return {
+        "n": tune.grid_search(data[Columns.system_size]),
+        "h": tune.grid_search(data[Columns.disorder]),
+        "chi": tune.grid_search(data[Columns.truncation_dim]),
+        "seed": tune.grid_search(
+            list(
+                range(
+                    data[Columns.seed]["start"],
+                    data[Columns.seed]["end"],
+                    data[Columns.seed]["step"],
+                )
+            )
+        ),
+        "penalty": data[Columns.penalty],
+        "s_target": data[Columns.s_target],
+        "offset": data[Columns.offset],
+        "overall_const": data[Columns.overall_const],
+    }
+
+
+@click.command()
+@click.option("-U", "--tracking_uri", type=str)
+@click.option(
+    "-P", "--artifact_path", default="/random_heisenberg/tsdrg/tree", type=str
+)
+@click.option(
+    "-N", "--experiment_name", default="random_heisenberg-tsdrg-energy_bounds", type=str
+)
+@click.option("--save_artifact", default=True, type=bool)
+@click.option("--num_cpus", default=16, type=int)
+@click.option("--cpu", default=1, type=int)
+@click.option("--memory", default=1, type=float, help="Memory size per task in GB.")
+def main(
+    tracking_uri: str,
+    artifact_path: str,
+    experiment_name: str,
+    save_artifact: bool,
+    num_cpus: int,
+    cpu: int,
+    memory: float,
+):
+    ray.init(num_cpus=num_cpus)
+    RandomHeisenbergTSDRGGridSearch(
+        tracking_uri=tracking_uri, local_path=os.getcwd(), artifact_path=artifact_path
+    ).run(
+        config=get_config(),
+        experiment_name=experiment_name,
+        save_artifact=save_artifact,
+        resources_per_trial={"cpu": cpu, "memory": memory * 1024 ** 3},
+    )
+
+
 if __name__ == "__main__":
-
-    penalty = 0.0
-    s_target = 0
-    offset = 0.0
-    n_conf = 500
-
-    params = [
-        {
-            'n': n,
-            'h': h,
-            'chi': chi,
-            'trial_id': trial_id,
-            'seed': seed,
-            'penalty': penalty,
-            's_target': s_target,
-            'offset': offset
-        }
-        for n in [8, 10, 12, 14, 16, 18, 20]
-        for chi in [2 ** 3, 2 ** 4, 2 ** 5, 2 ** 6]
-        for h in [0.5, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 10.0]
-        for trial_id, seed in enumerate(range(1900, 1900 + n_conf))
-    ]
+    main()
 
     # if "random_heisenberg" not in wr.catalog.databases().values:
     #     wr.catalog.create_database("random_heisenberg")
-
-    # cluster = LocalCluster(
-    #     n_workers=7,
-    #     threads_per_worker=1,
-    #     memory_limit="700MiB",
-    # )
-    ray.init(num_cpus=16)
-    results = Distributed.map_on_ray(main3, params)
     # print(wr.catalog.table(database="random_heisenberg", table="tsdrg"))
-    # merged_df = pd.concat(results)
-    # merged_df.to_parquet(f'~/data/random_heisenberg_tsdrg.parquet', index=False)
-    # merged_df.to_parquet(f'{Path(__file__).parents[1]}/data/random_heisenberg_tsdrg.parquet', index=False)
