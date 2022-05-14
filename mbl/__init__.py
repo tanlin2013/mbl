9c71715d8c36afe0ee2cd4bd183d191c819bf070
taolin <tanlin2013@gmail.com>
2022-05-14 15:39:45 +0800

    Build: add datawrangler config

--- mbl/__init__.py
+++ mbl/__init__.py
@@ -1,3 +1,17 @@
 from importlib import metadata
 
+import awswrangler as wr
+from botocore.config import Config
+
+
 __version__ = metadata.version(__name__)
+
+
+wr.config.botocore_config = Config(
+    retries={
+        "max_attempts": 3,
+        "mode": "standard"
+    },
+    connect_timeout=30,
+    max_pool_connections=20
+)
