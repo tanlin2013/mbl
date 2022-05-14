46917879e3b5be18f15096f53681427b8bfa4bb3
taolin <tanlin2013@gmail.com>
2022-05-14 16:09:48 +0800

    Test: migrate to pytest

--- tests/test_level_statistic.py
+++ tests/test_level_statistic.py
@@ -1,24 +1,53 @@
-from unittest import TestCase
-import pandas as pd
-from pathlib import Path
-from mbl.level_statistic import (
+import pytest
+import numpy as np
+
+from mbl.analysis.level_statistic import (
     LevelStatistic,
     AverageOrder
 )
 
 
-if __name__ == "__main__":
+class TestLevelStatistic:
 
-    N = 10
-    h = 1.0
-    total_sz = 0
+    @pytest.fixture(scope='class')
+    def level_statistic(self):
+        # raw_df = pd.read_csv(f'{Path(__file__).parent}/random_heisenberg_config.csv')
+        return LevelStatistic()
 
-    raw_df = pd.read_csv(f'{Path(__file__).parent}/random_heisenberg_config.csv')
-    agent = LevelStatistic(raw_df)
-    df = agent.extract_gap(N, h, total_sz=total_sz)
-    print(df.head(10))
+    def test_athena_query(self, level_statistic):
+        # df = wr.catalog.table(database="random_heisenberg", table="tsdrg")
+        df = level_statistic.athena_query(18, 0.5, chi=16, total_sz=0)
+        print(df)
 
-    r = agent.averaged_gap_ratio(df, AverageOrder.LevelFirst)
-    print(r)
-    r2 = agent.averaged_gap_ratio(df, AverageOrder.DisorderFirst)
-    print(r2)
+    def test_gap_ratio(self, level_statistic):
+        np.testing.assert_allclose(
+            level_statistic.gap_ratio(
+                np.array([np.nan, 0.00726675, 0.00550455, 0.01220888, 0.00061159, 0.00013423,
+                          0.00775276, 0.00357282, 0.04445829, 0.00015914])
+            ),
+            np.array([np.nan, 0.75749753, 0.45086416, 0.05009375, 0.21948545, 0.01731444,
+                      0.46084533, 0.08036352, 0.00357958, np.nan]),
+            atol=1e-5
+        )
+        np.testing.assert_array_equal(
+            level_statistic.gap_ratio(np.array([0.0478])),
+            np.nan
+        )
+        np.testing.assert_array_equal(
+            level_statistic.gap_ratio(np.array([-0.0478, 0.0478])),
+            np.array([-1, np.nan])
+        )
+
+    def test_extract_gap(self, level_statistic):
+        df = level_statistic.athena_query(8, 10.0, total_sz=0)
+        print(df)
+        # df2 = self.agent.extract_gap(20, 0.5, chi=64, total_sz=0, seed=1948)
+        # print(df2)
+        # df[Columns.level_id] = df.groupby([Columns.seed]).cumcount()
+
+    def test_averaged_gap_ratio(self, level_statistic):
+        df = level_statistic.extract_gap(n=10, h=1.0, total_sz=0)
+        r = level_statistic.averaged_gap_ratio(df, AverageOrder.LEVEL_FIRST)
+        print(r)
+        r2 = level_statistic.averaged_gap_ratio(df, AverageOrder.DISORDER_FIRST)
+        print(r2)
--- tests/test_random_heisenberg.py
+++ tests/test_random_heisenberg.py
@@ -1,131 +1,120 @@
-import unittest
+import pytest
 import numpy as np
 import pandas as pd
-from mbl.experiment import (
-    RandomHeisenbergED,
-    RandomHeisenbergTSDRG,
-    Columns
-)
+
+from mbl.name_space import Columns
+from mbl.schema import RandomHeisenbergEDSchema, RandomHeisenbergFoldingTSDRGSchema
+from mbl.experiment import RandomHeisenbergED, RandomHeisenbergFoldingTSDRG
 
 
-class TesRandomHeisenbergED(unittest.TestCase):
-
-    def __init__(self, *args, **kwargs):
-        super(TesRandomHeisenbergED, self).__init__(*args, **kwargs)
-        self.ham = np.array(
-            [[0.25, 0, 0, 0],
-             [0, -0.25, 0.5, 0],
-             [0, 0.5, -0.25, 0],
-             [0, 0, 0, 0.25]]
-        )
-        self.agent = RandomHeisenbergED(n=2, h=0, penalty=0, s_target=0, trial_id=0)
-
-    def test_matrix(self):
-        np.testing.assert_array_equal(self.agent.ed.matrix, self.ham)
-
-    def test_sorting_order(self):
-        self.assertIsNone(self.agent.sorting_order)
-
-    def test_evals(self):
-        np.testing.assert_array_equal(
-            self.agent.evals,
-            np.array([-0.75, 0.25, 0.25, 0.25])
+class TestRandomHeisenbergED:
+    @pytest.fixture(scope="class")
+    def ham(self):
+        return np.array(
+            [[0.25, 0, 0, 0], [0, -0.25, 0.5, 0], [0, 0.5, -0.25, 0], [0, 0, 0, 0.25]]
         )
 
-    def test_total_sz(self):
-        np.testing.assert_array_equal(
-            self.agent.total_sz,
-            np.array([0, 1, 0, -1])
-        )
+    @pytest.fixture(scope="class")
+    def agent(self):
+        return RandomHeisenbergED(n=2, h=0, penalty=0, s_target=0, trial_id=0)
 
-    def test_entanglement_entropy(self):
+    def test_matrix(self, agent, ham):
+        np.testing.assert_array_equal(agent.ed.matrix, ham)
+
+    def test_sorting_order(self, agent):
+        assert agent.sorting_order is None
+
+    def test_evals(self, agent):
+        np.testing.assert_array_equal(agent.evals, np.array([-0.75, 0.25, 0.25, 0.25]))
+
+    def test_total_sz(self, agent):
+        np.testing.assert_array_equal(agent.total_sz, np.array([0, 1, 0, -1]))
+
+    def test_entanglement_entropy(self, agent):
         np.testing.assert_allclose(
-            self.agent.entanglement_entropy(0),
+            agent.entanglement_entropy(0),
             np.array([np.log(2), np.nan, np.log(2), np.nan]),
-            atol=1e-12
+            atol=1e-12,
         )
 
-    def test_df(self):
+    def test_compute_df(self, agent):
+        df = agent.compute_df()
         pd.testing.assert_frame_equal(
             pd.DataFrame(
                 {
                     Columns.level_id: list(range(4)),
                     Columns.en: [-0.75, 0.25, 0.25, 0.25],
-                    Columns.total_sz: [0., 1., 0., -1.],
+                    Columns.total_sz: [0.0, 1.0, 0.0, -1.0],
                     Columns.edge_entropy: [np.log(2), np.nan, np.log(2), np.nan],
                     Columns.bipartite_entropy: [np.log(2), np.nan, np.log(2), np.nan],
                     Columns.system_size: [2, 2, 2, 2],
                     Columns.disorder: [0, 0, 0, 0],
                     Columns.trial_id: [0, 0, 0, 0],
-                    Columns.seed: [self.agent.model.seed] * 4,
+                    Columns.seed: [agent.model.seed] * 4,
                     Columns.penalty: [0, 0, 0, 0],
                     Columns.s_target: [0, 0, 0, 0],
-                    Columns.offset: [0, 0, 0, 0]
+                    Columns.offset: [0, 0, 0, 0],
                 }
             ),
-            self.agent.df
+            df,
         )
+        RandomHeisenbergEDSchema.validate(df, lazy=True)
 
 
-class TestRandomHeisenbergTSDRG(unittest.TestCase):
+class TestRandomHeisenbergFoldingTSDRG:
+    @pytest.fixture(scope="class")
+    def agent(self):
+        return RandomHeisenbergFoldingTSDRG(n=6, h=0.5, chi=2 ** 6)
 
-    def __init__(self, *args, **kwargs):
-        super(TestRandomHeisenbergTSDRG, self).__init__(*args, **kwargs)
-        self.agent = RandomHeisenbergTSDRG(
-            n=6, h=0.5, chi=2**6
-        )
-        self.ed_agent = RandomHeisenbergED(
-            self.agent.model.n, self.agent.model.h, seed=self.agent.model.seed
-        )
+    @pytest.fixture(scope="class")
+    def ed_agent(self, agent):
+        return RandomHeisenbergED(agent.model.n, agent.model.h, seed=agent.model.seed)
 
-    def test_evals(self):
+    def test_evals(self, agent, ed_agent):
+        np.testing.assert_allclose(agent.evals, ed_agent.evals, atol=1e-12)
+
+    def test_variance(self, agent):
         np.testing.assert_allclose(
-            self.agent.evals,
-            self.ed_agent.evals,
-            atol=1e-12
+            agent.variance, np.zeros(agent.tsdrg.chi), atol=1e-12
         )
 
-    def test_variance(self):
+    def test_total_sz(self, agent, ed_agent):
+        np.testing.assert_allclose(agent.total_sz, ed_agent.total_sz, atol=1e-12)
+
+    def test_edge_entropy(self, agent, ed_agent):
         np.testing.assert_allclose(
-            self.agent.variance,
-            np.zeros(self.agent.tsdrg.chi),
-            atol=1e-12
+            agent.edge_entropy,
+            np.nan_to_num(ed_agent.entanglement_entropy(0)),
+            atol=1e-12,
         )
 
-    def test_total_sz(self):
-        np.testing.assert_allclose(
-            self.agent.total_sz,
-            self.ed_agent.total_sz,
-            atol=1e-12
-        )
-
-    def test_edge_entropy(self):
-        np.testing.assert_allclose(
-            self.agent.edge_entropy,
-            np.nan_to_num(self.ed_agent.entanglement_entropy(0)),
-            atol=1e-12
-        )
-
-    def test_df(self):
-        n_row = self.agent.tsdrg.chi
+    def test_compute_df(self, agent, ed_agent):
+        n_row = agent.tsdrg.chi
+        df = agent.compute_df()
         pd.testing.assert_frame_equal(
             pd.DataFrame(
                 {
                     Columns.level_id: list(range(n_row)),
-                    Columns.en: self.ed_agent.evals.tolist(),
+                    Columns.en: ed_agent.evals.tolist(),
                     Columns.variance: np.zeros(n_row),
-                    Columns.total_sz: self.ed_agent.total_sz.tolist(),
-                    Columns.edge_entropy: np.nan_to_num(self.ed_agent.entanglement_entropy(0)).tolist(),
-                    Columns.truncation_dim: [self.agent.tsdrg.chi] * n_row,
-                    Columns.system_size: [self.agent.model.n] * n_row,
-                    Columns.disorder: [self.agent.model.h] * n_row,
-                    Columns.trial_id: [self.agent.model.trial_id] * n_row,
-                    Columns.seed: [self.agent.model.seed] * n_row,
-                    Columns.penalty: [self.agent.folded_model.penalty] * n_row,
-                    Columns.s_target: [self.agent.folded_model.s_target] * n_row,
-                    Columns.offset: [self.agent.folded_model.offset] * n_row
+                    Columns.total_sz: ed_agent.total_sz.tolist(),
+                    Columns.edge_entropy: np.nan_to_num(
+                        ed_agent.entanglement_entropy(0)
+                    ).tolist(),
+                    Columns.truncation_dim: [agent.tsdrg.chi] * n_row,
+                    Columns.system_size: [agent.model.n] * n_row,
+                    Columns.disorder: [agent.model.h] * n_row,
+                    Columns.trial_id: [agent.model.trial_id] * n_row,
+                    Columns.seed: [agent.model.seed] * n_row,
+                    Columns.penalty: [0] * n_row,
+                    Columns.s_target: [0] * n_row,
+                    Columns.offset: [0] * n_row,
+                    Columns.max_en: [0] * n_row,
+                    Columns.min_en: [0] * n_row,
+                    Columns.relative_offset: [0] * n_row,
                 }
             ),
-            self.agent.df,
-            atol=1e-12
+            df,
+            atol=1e-12,
         )
+        RandomHeisenbergFoldingTSDRGSchema.validate(df, lazy=True)
