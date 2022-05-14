8ca2c0bbd6e439d2263d5271735600b95c79e1f7
taolin <tanlin2013@gmail.com>
2022-05-14 15:26:54 +0800

    Feature: update schema model

--- mbl/schema.py
+++ mbl/schema.py
@@ -1,21 +1,20 @@
 import numpy as np
-import pandas as pd
 import pandera as pa
 from pandera.typing import Series
 
 from mbl.name_space import Columns
 
 
-class EDSchema(pa.SchemaModel):
+class RandomHeisenbergEDSchema(pa.SchemaModel):
 
     level_id: Series[int] = pa.Field(ge=0)
     en: Series[float]
     total_sz: Series[float]
-    edge_entropy: Series[float] = pa.Field(ge=0, le=np.log(2), nullable=True)
-    bipartite_entropy: Series[float] = pa.Field(ge=0, le=np.log(2), nullable=True)
+    edge_entropy: Series[float] = pa.Field(nullable=True)
+    bipartite_entropy: Series[float] = pa.Field(nullable=True)
     system_size: Series[int] = pa.Field(gt=1)
     disorder: Series[float] = pa.Field(coerce=True)
-    trial_id: Series[pd.Int64Dtype] = pa.Field(nullable=True, coerce=True)
+    trial_id: Series[str] = pa.Field(nullable=True)
     seed: Series[int]
     penalty: Series[float] = pa.Field(coerce=True)
     s_target: Series[int]
@@ -25,23 +24,63 @@ class EDSchema(pa.SchemaModel):
     def close_to_integer(cls, series: Series[float]) -> Series[bool]:
         return Series(np.isclose(series, np.rint(series), atol=1e-12))
 
+    @pa.check(Columns.edge_entropy)
+    def bound_in(cls, series: Series[float]) -> Series[bool]:
+        return (-1e-12 < series) & (series < np.log(2) + 1e-12)
 
-class TSDRGSchema(pa.SchemaModel):
+    @pa.check(Columns.bipartite_entropy)
+    def bound_in(cls, series: Series[float]) -> Series[bool]:
+        return (-1e-12 < series) & (series < np.log(2) + 1e-12)
+
+
+class RandomHeisenbergTSDRGSchema(pa.SchemaModel):
 
     level_id: Series[int] = pa.Field(ge=0)
     en: Series[float]
     variance: Series[float]
     total_sz: Series[float]
-    edge_entropy: Series[float] = pa.Field(ge=0, le=np.log(2), nullable=True)
+    edge_entropy: Series[float] = pa.Field(nullable=True)
     truncation_dim: Series[int]
     system_size: Series[int] = pa.Field(gt=1)
     disorder: Series[float] = pa.Field(coerce=True)
-    trial_id: Series[pd.Int64Dtype] = pa.Field(nullable=True, coerce=True)
+    trial_id: Series[str] = pa.Field(nullable=True)
     seed: Series[int]
     penalty: Series[float] = pa.Field(coerce=True)
     s_target: Series[int]
-    offset: Series[float] = pa.Field(ge=0, le=1, coerce=True)
+    offset: Series[float] = pa.Field(coerce=True)
+    overall_const: Series[float]
 
     @pa.check(Columns.total_sz)
     def close_to_integer(cls, series: Series[float]) -> Series[bool]:
         return Series(np.isclose(series, np.rint(series), atol=1e-12))
+
+    @pa.check(Columns.edge_entropy)
+    def bound_in(cls, series: Series[float]) -> Series[bool]:
+        return (-1e-12 < series) & (series < np.log(2) + 1e-12)
+
+
+class RandomHeisenbergFoldingTSDRGSchema(pa.SchemaModel):
+    level_id: Series[int] = pa.Field(ge=0)
+    en: Series[float]
+    variance: Series[float]
+    total_sz: Series[float]
+    edge_entropy: Series[float] = pa.Field(nullable=True)
+    truncation_dim: Series[int]
+    system_size: Series[int] = pa.Field(gt=1)
+    disorder: Series[float] = pa.Field(coerce=True)
+    trial_id: Series[str] = pa.Field(nullable=True)
+    seed: Series[int]
+    penalty: Series[float] = pa.Field(coerce=True)
+    s_target: Series[int]
+    offset: Series[float] = pa.Field(coerce=True)
+    max_en: Series[float] = pa.Field(coerce=True)
+    min_en: Series[float] = pa.Field(coerce=True)
+    relative_offset: Series[float] = pa.Field(ge=0, le=1, coerce=True)
+
+    @pa.check(Columns.total_sz)
+    def close_to_integer(cls, series: Series[float]) -> Series[bool]:
+        return Series(np.isclose(series, np.rint(series), atol=1e-12))
+
+    @pa.check(Columns.edge_entropy)
+    def bound_in(cls, series: Series[float]) -> Series[bool]:
+        return (-1e-12 < series) & (series < np.log(2) + 1e-12)
