c4683afd933df493a5273bb0cd231a16607ca3d2
taolin <tanlin2013@gmail.com>
2022-05-23 17:04:21 +0800

    Fix: follow up fixes in gridsearch

--- scripts/sampler.py
+++ scripts/sampler.py
@@ -7,7 +7,7 @@ import ray
 from ray import tune
 
 from mbl.name_space import Columns
-from mbl.pipeline.grid_search import (
+from mbl.workflow.grid_search import (
     run,
     RandomHeisenbergTSDRGGridSearch,
     RandomHeisenbergFoldingTSDRGGridSearch,
@@ -33,62 +33,61 @@ def scopion():
     )
 
 
-def config_parser(local_path: str, artifact_path: str):
+def config_parser():
     with open(Path(__file__).parent / "run_config.yml", "r", encoding="utf-8") as f:
         data = yaml.load(f, Loader=yaml.Loader)
-    return {
-        "n": tune.grid_search(data[Columns.system_size]),
-        "h": tune.grid_search(data[Columns.disorder]),
-        "chi": tune.grid_search(data[Columns.truncation_dim]),
-        "seed": tune.grid_search(
-            list(
-                range(
-                    data[Columns.seed]["start"],
-                    data[Columns.seed]["end"],
-                    data[Columns.seed]["step"],
-                )
-            )
-        ),
+    config = {
+        "n": data[Columns.system_size],
+        "h": data[Columns.disorder],
+        "chi": data[Columns.truncation_dim],
+        "seed": list(range(*data[Columns.seed].values())),
         "penalty": data[Columns.penalty],
         "s_target": data[Columns.s_target],
         "offset": data[Columns.offset],
         "overall_const": data[Columns.overall_const],
-        "local_path": local_path,
-        "artifact_path": artifact_path,
     }
+    return {k: tune.grid_search(v) for k, v in config.items()}
 
 
 @click.command()
-@click.option("-U", "--tracking_uri", type=str)
-@click.option(
-    "-P", "--artifact_path", default="/random_heisenberg/tsdrg/tree", type=str
-)
+@click.option("-U", "--tracking_uri", default="http://localhost:8787", type=str)
+@click.option("-E", "--mlflow_s3_endpoint", default="http://localhost:9000", type=str)
 @click.option(
     "-N", "--experiment_name", default="random_heisenberg-tsdrg-energy_bounds", type=str
 )
-@click.option("--save_artifact", default=True, type=bool)
 @click.option(
     "--num_cpus", default=16, type=int, help="Number of total available cpus."
 )
 @click.option("--cpu", default=1, type=int, help="Number of cpu per task.")
 @click.option("--memory", default=1, type=float, help="Memory size per task in GB.")
+@click.option(
+    "--verbose",
+    default=3,
+    type=int,
+    help="0, 1, 2, or 3. Verbosity mode. "
+         "0 = silent, "
+         "1 = only status updates, "
+         "2 = status and brief trial results, "
+         "3 = status and detailed trial results",
+)
 def main(
     tracking_uri: str,
-    artifact_path: str,
+    mlflow_s3_endpoint: str,
     experiment_name: str,
-    save_artifact: bool,
     num_cpus: int,
     cpu: int,
     memory: float,
+    verbose: int,
 ):
-    ray.init(num_cpus=num_cpus)
+    os.environ["MLFLOW_S3_ENDPOINT_URL"] = mlflow_s3_endpoint
+    ray.init(num_cpus=num_cpus, object_store_memory=memory * 1024 ** 3)
     run(
-        experiment=RandomHeisenbergTSDRGGridSearch,
-        config=config_parser(local_path=os.getcwd(), artifact_path=artifact_path),
         tracking_uri=tracking_uri,
         experiment_name=experiment_name,
-        save_artifact=save_artifact,
-        resources_per_trial={"cpu": cpu, "memory": memory * 1024 ** 3},
+        experiment=RandomHeisenbergTSDRGGridSearch.experiment,
+        configs=config_parser(),
+        resources_per_trial={"cpu": cpu},
+        verbose=verbose,
     )
 
 
