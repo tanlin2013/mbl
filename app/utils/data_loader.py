00c1486910af25cecec8dda95a03767e3f9a9dd2
taolin <tanlin2013@gmail.com>
2022-06-18 17:22:33 +0800

    Refactor: update accordingly with mbl

--- app/utils/data_loader.py
+++ app/utils/data_loader.py
@@ -1,11 +1,11 @@
-import orjson
+import orjson  # noqa: F401
 import numpy as np
 import pandas as pd
 import awswrangler as wr
 import streamlit as st
 
 from mbl.name_space import Columns
-from mbl.analysis.level_statistic import LevelStatistic
+from mbl.analysis.level_statistic import LevelStatistic, AverageOrder
 from mbl.analysis.energy_bounds import EnergyBounds
 
 
@@ -17,42 +17,45 @@ def load_data(
     relative_offset: float = None,
     total_sz: int = None,
 ) -> pd.DataFrame:
-    df = LevelStatistic().extract_gap(
+    df = LevelStatistic.athena_query(
         n, h, chi=chi, relative_offset=relative_offset, total_sz=total_sz
     )
+    df = LevelStatistic.extract_gap(df)
     if chi is not None:
         df["error"] = np.sqrt(df[Columns.variance].to_numpy() / chi)
-    return df
+    return df._to_pandas()
 
 
 @st.cache(persist=True)
-def fetch_gap_ratio(n: int, h: float, chi: int = None, total_sz: int = None) -> float:
-    return LevelStatistic().averaged_gap_ratio(load_data(**locals()))
+def fetch_gap_ratio(
+    n: int,
+    h: float,
+    chi: int = None,
+    total_sz: int = None,
+    order: AverageOrder = AverageOrder.LEVEL_FIRST,
+) -> float:
+    return LevelStatistic.fetch_gap_ratio(**locals())
 
 
 @st.cache(persist=True)
-def fetch_energy_bounds():
-    items = [
-        Columns.system_size,
-        Columns.disorder,
-        Columns.truncation_dim,
-        Columns.seed,
-        f"MIN({Columns.en}) AS min_en",
-        Columns.overall_const,
-        Columns.total_sz,
-    ]
+def fetch_energy_bounds(method: str):
     groups = [
         Columns.system_size,
         Columns.disorder,
-        Columns.truncation_dim,
         Columns.seed,
         Columns.overall_const,
+        Columns.truncation_dim,
     ]
+    assert method in ["min", "max"]
     return wr.athena.read_sql_query(
         f"""
-        SELECT {', '.join(items)}
+        SELECT *
         FROM {EnergyBounds.Metadata.table}
-        GROUP BY {', '.join(groups)}
+        WHERE {Columns.en} IN (
+            SELECT {method.upper()}({Columns.en})
+            FROM {EnergyBounds.Metadata.table}
+            GROUP BY {', '.join(groups)}
+        )
         """,
         database=EnergyBounds.Metadata.database,
     ).sort_values(
