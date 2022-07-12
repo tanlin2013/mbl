1eb4ac2b698519046ed258a941cfac4737419d7f
taolin <tanlin2013@gmail.com>
2022-07-12 15:47:25 +0800

    Build: configure sphinx setting

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,6 +15,7 @@ import sys
 
 sys.path.insert(0, os.path.abspath("."))
 sys.path.insert(0, os.path.abspath("../../mbl"))
+from mbl import __version__  # noqa: E402
 
 
 # -- Project information -----------------------------------------------------
@@ -23,27 +24,58 @@ project = "mbl"
 copyright = "2022, Tan Tao-Lin"
 author = "Tan Tao-Lin"
 
+# The short X.Y version
+version = __version__
+# The full version, including alpha/beta/rc tags
+release = __version__
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",
+    "nbsphinx",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.doctest",
+    "sphinx.ext.todo",
+    "sphinx.ext.coverage",
+    "sphinx.ext.ifconfig",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.githubpages",
+    "sphinx.ext.inheritance_diagram",
+    "m2r2",
+]
+
+# Turn on sphinx.ext.autosummary
+autodoc_default_options = {
+    "members": True,
+    "member-order": "bysource",
+    "special-members": "__init__",
+    "undoc-members": True,
+    "exclude-members": "__weakref__",
+    "autosummary": True,
+    "show-inheritance": True,
+}
+autodoc_typehints = "description"
 
 # Mathjax
-mathjax_path = (
-    "https://cdn.jsdelivr.net/npm/mathjax@2/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
-)
-mathjax2_config = {
-    "tex2jax": {
-        "inlineMath": [["$", "$"], ["\\(", "\\)"]],
-        "displayMath": [["$$", "$$"]],
-        "processEscapes": True,
-        "ignoreClass": "document",
-        "processClass": "math|output_area",
-    }
-}
+# mathjax_path = (
+#     "https://cdn.jsdelivr.net/npm/mathjax@2/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
+# )
+# mathjax2_config = {
+#     "tex2jax": {
+#         "inlineMath": [["$", "$"], ["\\(", "\\)"]],
+#         "displayMath": [["$$", "$$"]],
+#         "processEscapes": True,
+#         "ignoreClass": "document",
+#         "processClass": "math|output_area",
+#     }
+# }
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -53,13 +85,16 @@ templates_path = ["_templates"]
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+# Allowing docstring in both __init__ and right under class definition
+autoclass_content = "both"
+
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = "sphinx_book_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
